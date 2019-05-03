44b6cdeec4c67f720ce99a26c61cf9be78096933
Hiroya Komine <hk0mine@outlook.jp>
2019-05-03 17:54:13 +0900

    Add script for sending post

---
+++ post.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from slacker import Slacker
+
+if __name__ == '__main__':
+  slack = Slacker(os.environ['SLACKBOT_API_TOKEN'])
+  slack.chat.post_message(
+    'private',
+    sys.argv[1],
+    as_user=True
+    )
