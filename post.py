ce0d2b68acc7356ebafb4e6d481ffbf55b26457a
h.komine <hk0mine@outlook.jp>
2019-05-04 11:36:09 +0900

    Use argv[1] for channel to send a post

--- post.py
+++ post.py
@@ -5,7 +5,7 @@ from slacker import Slacker
 if __name__ == '__main__':
   slack = Slacker(os.environ['SLACKBOT_API_TOKEN'])
   slack.chat.post_message(
-    'private',
     sys.argv[1],
+    sys.argv[2],
     as_user=True
     )
