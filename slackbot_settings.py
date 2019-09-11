b2ec62c67a473738f6bb533eda6ef14688a34b27
H.Komine <hk0mine@outlook.jp>
2019-09-11 23:10:35 +0900

    Fix slackbot_settings.py

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-#API_TOKEN: uss environment variable 'SLACKBOT_API_TOKEN'
+# Set API Token for your bot
+API_TOKEN = "xxxx-xxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx"
 
 DEFAULT_REPLY = "?????"
 
